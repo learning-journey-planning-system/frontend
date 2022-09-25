71105a8aec283b51f4b7e678b5b878e023fd1dce
aiweiii <aiwei.ong.2020@scis.smu.edu.sg>
2022-09-25 12:52:22 +0800

    added bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+Vue.use(BootstrapVue);
 
 createApp(App).mount('#app')
