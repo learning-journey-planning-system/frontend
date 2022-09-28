aa711582c1d9f3e4c3176ec2fce3be3f49a0aefa
jiayii01 <79521323+jiayii01@users.noreply.github.com>
2022-09-28 23:46:43 +0800

    SPM-19 created role cards

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,8 +1,28 @@
 <template>
   <NavBar/>
-  <div class="container">
-    <h1>Welcome Learner</h1>
+    <div class="album py-5">
+    <div class="container">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="col" v-for="role in roles" :key=role.id>
+          <div class="card shadow-sm">
+            <div class="card-body">
+              <h5 class="card-title">{{role.title}}</h5>
+              <p class="card-text"></p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+
+
 </template>
 
 <script>
@@ -12,6 +32,16 @@ export default {
   name: 'App',
   components: {
     NavBar,
+  },
+  data(){
+    return{
+      roles: [
+        {title: 'Data Analyst', id: 1, status: 'Inactive'},
+        {title: 'Data Scientist', id: 2, status: 'Active'},
+        {title: 'Data Engineer', id: 3, status: 'Active'},
+        {title: 'Machine Learning Engineer', id: 4, details: 'Active'},
+      ]
+    }
   }
 }
 
