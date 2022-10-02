859c1294750ed2b5ed9ee03d3c925ead2d9d0656
glxnd <80815026+glxnd@users.noreply.github.com>
2022-10-03 02:08:33 +0800

    Tested

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <TheNavbar />
+  <SkillCard  SkillName = "Javascript" deleted="false" SkillID = "1" SkillURL = "http://www.google.com"></SkillCard>
 </template>
 
 <script>
 import TheNavbar from './components/TheNavbar.vue';
+import SkillCard from './components/SkillCard.vue';
+
 
 export default {
   name: 'App',
   components: {
     TheNavbar,
+    SkillCard,
 }
 }
 </script>
