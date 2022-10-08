02af7844751458a135a8a17269e9fde3d45508a7
glxnd <80815026+glxnd@users.noreply.github.com>
2022-10-08 15:56:56 +0800

    Updated Interface

--- src/views/APanelViewRoles.vue
+++ src/views/APanelViewRoles.vue
@@ -12,17 +12,17 @@
       <div class="col-4">
         <div class="card shadow-sm">
           <div class="card-body" style = "padding-bottom:5px;">
-            <h5 class="card-title">{{role.jobrole_name}}</h5>
+            <div class = "d-flex justify-content-between "> <h5 class="card-title">{{role.jobrole_name}}</h5> </div>
             <p class = "d-flex justify-content-between align-items-center">
                 Role ID: {{role.id}}
-              <small class="text-muted"><button v-if="role.deleted == true" type="button" class="btn btn-sm btn-outline-danger disabled">Deleted</button></small>
+              <small class="text-muted"><button v-if="role.deleted == true" type="button" class="btn btn-sm btn-outline-danger disabled">Deleted</button> <button v-else disabled type="button" class="btn btn-outline-success">Active</button></small>
             </p> 
             <br>
             <h6>Assign Skills</h6>
             <form @submit.prevent = "onSubmit(role.id)" >
               <select class="form-select" aria-label="Default select example" v-model = "skillchoices" multiple >
                   <option disabled>Select Skills Here</option>
-                  <option v-for="skill in skills"  v-bind:key = skill.id v-bind:value= skill.id> {{skill.skill_name}} </option>
+                  <option v-for="skill in skills"   v-bind:key = skill.id v-bind:value= skill.id> {{skill.skill_name}} </option>
                 </select>
             
               <p class = "d-flex justify-content-between" style = "padding-top:10px">
