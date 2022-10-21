437b165556fbc84501748a2bab4c0739b5b2c3d6
aiweiii <aiwei.ong.2020@scis.smu.edu.sg>
2022-10-21 12:41:53 +0800

    users are ONLY allowed to choose a job role where - learning journey for this job role has not been created - this course is applicable to the job role (because both have skills that matches)

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,3 +1,5 @@
+<!-- THIS FILE IS A BACKUP. AIWEI WILL DELETE ONCE ALL TEST CASES PASS -->
+
 <template>
   <div class="col-3 align-self-center" style="text-align: right;">
     <!-- Button trigger modal -->
@@ -18,7 +20,8 @@
               <!-- form select -->
               <h5>Select a Job Role:</h5>
               <select class="form-select" size="8" v-model="selected">
-                <option v-for="jobRole in this.allAvailableJobRolesForCourses" :key="jobRole" :value=jobRole.id>{{jobRole.jobrole_name}}</option>
+                <option v-for="jobRole in this.allAvailableRolesForThisCourse" :key="jobRole" :value=jobRole.id>
+                  {{jobRole.jobrole_name}}</option>
               </select>
             </div>
             <div class="modal-footer">
@@ -38,17 +41,15 @@ import axios from 'axios'
 
 export default {
   name: 'CreateLearningJourney',
-  data(){
+  data() {
     return {
-      allAvailableJobRoles: [],
-      allAvailableJobRolesForCourses: [],
       staffLearningJourneys: null,
-      staffExistingJobRolesInLJs: [],
+      allAvailableRolesForThisCourse: [],
       selected: ''
     }
   },
   methods: {
-    submitForm(){
+    submitForm() {
       // console.log("job role id here: ",this.selected);
       axios
         .post("http://127.0.0.1:8000/api/v1/learningjourney/", {
@@ -66,10 +67,25 @@ export default {
           }
         })
     },
-    isCourseApplicableForJobRole(cID, jobRole){
+    getAllAvailableRoles: async (staffExistingJobRolesInLJs) => {
+      var allAvailableJobRoles = [];
+      try {
+        let response = await axios.get("http://127.0.0.1:8000/api/v1/jobrole/available/")
+        for (let i = 0; i < response.data.length; i++) {
+          if (!staffExistingJobRolesInLJs.includes(response.data[i].id)) {
+            allAvailableJobRoles.push(response.data[i]);
+          }
+        }
+        console.log("original available job roles NOT in user's learning journeys", allAvailableJobRoles)
+        return allAvailableJobRoles
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    isCourseApplicableForJobRole(cID, jobRoleId) {
       // var allCoursesFromJobRole = [];
       console.log("course id: ", cID);
-      console.log("job role: ", jobRole)
+      console.log("job role id: ", jobRoleId)
       var activeSkillsForCourse = [];
       var activeSkillsForJobRole = [];
 
@@ -80,53 +96,65 @@ export default {
           for (let i = 0; i < response.data.length; i++) {
             activeSkillsForCourse.push(response.data[i].id)
           }
+          console.log('active skills for COURSE: ', activeSkillsForCourse)
           // 2. for jobRoleId, get skills related to it, store into activeSkillsForJobRole
           axios
-            .get(`http://127.0.0.1:8000/api/v1/jobrole/${jobRole.id}/available_skills/`)
+            .get(`http://127.0.0.1:8000/api/v1/jobrole/${jobRoleId}/available_skills/`)
             .then((response) => {
               for (let i = 0; i < response.data.length; i++) {
                 activeSkillsForJobRole.push(response.data[i].id)
               }
+              console.log('active skills for JOB ROLE: ', activeSkillsForJobRole)
               const intersection = activeSkillsForCourse.filter(element => activeSkillsForJobRole.includes(element));
-              if (intersection.length>0) {
+              if (intersection.length > 0) {
                 // course is applicable for job role
-                this.allAvailableJobRolesForCourses.push(jobRole)
+                return true
               } else {
-                console.log('false')
+                return false
               }
             })
         })
     }
   },
-  mounted: function(){
+  mounted: function () {
+    var staffExistingJobRolesInLJs = [];
+    var allAvailableRoles = [];
+
     axios
       .get(`http://127.0.0.1:8000/api/v1/staff/${sessionStorage.getItem('staffID')}/learningjourneys`)
       .then((response) => {
         this.staffLearningJourneys = response.data;
         for (let i = 0; i < this.staffLearningJourneys.length; i++) {
-          this.staffExistingJobRolesInLJs.push(this.staffLearningJourneys[i].jobrole_id)
+          staffExistingJobRolesInLJs.push(this.staffLearningJourneys[i].jobrole_id)
         }
-        console.log("job roles in learning journeys: ", this.staffExistingJobRolesInLJs)
-        axios
-          .get("http://127.0.0.1:8000/api/v1/jobrole/available/")
-          .then((response) => {
-            for (let i = 0; i < response.data.length; i++) {
-              if (!this.staffExistingJobRolesInLJs.includes(response.data[i].id)) {
-                this.allAvailableJobRoles.push(response.data[i]);
+        console.log("job roles in learning journeys: ", staffExistingJobRolesInLJs)
+        allAvailableRoles = this.getAllAvailableRoles(staffExistingJobRolesInLJs)
+        console.log(allAvailableRoles)
 
-              }
-            }
-            console.log("original available job roles NOT in user's learning journeys",this.allAvailableJobRoles)
-            for (let i = 0; i < this.allAvailableJobRoles.length; i++) {
-              this.isCourseApplicableForJobRole(sessionStorage.getItem("courseID"),this.allAvailableJobRoles[i])
+        for (let i = 0; i < allAvailableRoles.length; i++) {
+          console.log('hi')
+          console.log(this.isCourseApplicableForJobRole(sessionStorage.getItem("courseID"), allAvailableRoles[i].id))
+          // if (this.isCourseApplicableForJobRole(sessionStorage.getItem("courseID"), allAvailableRoles[i].id)) {
+          //   this.allAvailableRolesForThisCourse.push(allAvailableRoles[i].id)
+          // }
 
-            }
-
-          })
-          console.log("all job roles that this user can pick from: ",this.allAvailableJobRoles)
+        }
+        // axios
+        //   .get("http://127.0.0.1:8000/api/v1/jobrole/available/")
+        //   .then((response) => {
+        //     console.log("ALL ROLES: ",response.data);
+        //     for (let i = 0; i < response.data.length; i++) {
+        //       if (!this.staffExistingJobRolesInLJs.includes(response.data[i].id)) {
+        //         this.allAvailableJobRoles.push(response.data[i]);
+        //       }
+        //     }
+        //     console.log("original available job roles NOT in user's learning journeys",this.allAvailableJobRoles)
+        // })
+        // console.log("all job roles that this user can pick from: ",this.allAvailableJobRoles)
       })
   }
 
 
 }
 </script>
+
