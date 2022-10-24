d203c0821919e04cddeaa8454fef1487c9a79946
aiweiii <aiwei.ong.2020@scis.smu.edu.sg>
2022-10-25 02:59:06 +0800

    added button to delete all learning journeys

--- src/views/LLearningJourney.vue
+++ src/views/LLearningJourney.vue
@@ -2,7 +2,15 @@
   <NavBar />
   <BackMiniNav></BackMiniNav>
   <div class="container">
-    <div class="row"><h2 class="fs-2 p-3">My Learning Journeys</h2></div>
+    <div class="row">
+      <div class="col">
+        <h2 class="fs-2 p-3">My Learning Journeys</h2>
+      </div>
+      <div class="col align-self-center" style="text-align: right;">
+        <button @click="deleteAllLearningJourneys()" type="button" class="btn btn-danger btn-sm">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill mb-1 me-1" viewBox="0 0 16 16"><path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/></svg>Delete <strong>ALL</strong> Learning Journeys</button>
+      </div>
+    </div>
     <div class="row">
       <!-- accordion starts here -->
       <div class="accordion" id="accordionPanelsStayOpenExample">
@@ -105,6 +113,16 @@ export default {
         this.$router.push({ name: 'LViewSkills', params: { roleID: roleDetails[0], rolename: roleDetails[2] } });
       }
     },
+    deleteAllLearningJourneys(){
+      if (confirm("All learning journey progress will be PERMANENTLY DELETED\nDo you still wish to proceed?")) {
+        for (let i = 0; i < this.staffLearningJourneys.length; i++) {
+          axios
+            .delete(`http://127.0.0.1:8000/api/v1/learningjourney/${this.staffLearningJourneys[i].id}`)
+        }
+        window.location.reload();
+        alert("All learning journeys are successfully deleted!")
+      }
+    }
   },
   mounted() {
     axios
