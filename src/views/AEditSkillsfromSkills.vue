e8402f19b8340d4849cf468e9e91301a5a5e8da3
jiayii01 <79521323+jiayii01@users.noreply.github.com>
2022-10-24 17:49:40 +0800

    SPM-102 added edit link to each skill for admins

---
+++ src/views/AEditSkillsfromSkills.vue
@@ -0,0 +1,64 @@
+<template>
+  <NavBar/>
+  <BackMiniNav message="Viewing Courses"/>
+  <div class="container">
+    <h2 class="mt-2">Edit {{skillName}} Skill</h2>
+      <form>
+        <div class="mb-2">
+          <label>Skill Name</label>
+          <input class="form-control" v-model="editedSkillName" placeholder="Enter Skill Name">
+          <div class="form-text">
+            Please capitalize the first letter of each word
+          </div>
+        </div>
+        <button @click="editSkills()" type="button" class="btn btn-success mt-2">Submit</button>
+      </form>
+  </div>
+</template>
+  
+<script>
+import NavBar from '../components/NavBar.vue';
+import BackMiniNav from '../components/BackMiniNav.vue';
+import axios from 'axios'
+
+export default {
+  components: {
+    NavBar,
+    BackMiniNav,
+  },
+  props: ['skillID', 'skillName'],
+  data(){
+    return{
+      editedSkillName: null,
+      allSkills: []
+    }
+  },
+  created() {
+    axios
+    .get("http://127.0.0.1:8000/api/v1/skill/")
+    .then((response) => {
+      response.data.forEach(ele => this.allSkills.push(ele.skill_name))
+    })
+  },
+  methods:{
+      editSkills(){
+        if(!this.allSkills.includes(this.editedSkillName)){
+          axios
+          .put(`http://127.0.0.1:8000/api/v1/skill/${this.skillID}`, {
+            skill_name: this.editedSkillName
+          })
+          .then(function(response){
+            console.log(response)
+            alert("Skill has been successfully edited!");
+            history.back()
+          })
+      } else{
+        alert("Skill with this skill name already exists")
+      }
+    },
+  }
+}
+</script>
+
+<style>
+</style>
