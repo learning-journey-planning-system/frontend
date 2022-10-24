25e6be331790b05510be795d532e2fc9c2355d98
aiweiii <aiwei.ong.2020@scis.smu.edu.sg>
2022-10-25 02:44:50 +0800

    added redirection icon to redirect users view job role's skills and courses from learning journey page

--- src/views/LLearningJourney.vue
+++ src/views/LLearningJourney.vue
@@ -15,7 +15,12 @@
           <div :id="'panelsStayOpen-collapse'+learningjourney.id" class="accordion-collapse collapse">
             <div class="accordion-body">
               <div class="container">
-                <h4 class="mt-1 mb-4">{{learningjourney.jobrole.jobrole_name}}</h4>
+                <h4 class="mt-1 mb-4">
+                  {{learningjourney.jobrole.jobrole_name}}
+                  <a href="" class="link-primary" @click="sendRoleData([learningjourney.jobrole.id, learningjourney.jobrole.deleted, learningjourney.jobrole.jobrole_name])">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/><path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/></svg>
+                  </a>
+                </h4>
                 <!-- display all skills for this learning journey -->
                 <button v-for="skill in allSkills(learningjourney)" :key="skill" :class=indicateStatus(skill.deleted) v-show=toShow(skill.deleted) type="button" style="width:150px; height: 65px;">{{skill.skill_name}}</button>
 
@@ -94,6 +99,11 @@ export default {
         coursesForThisLJ.push(lj.courses[i])
       }
       return coursesForThisLJ
+    },
+    sendRoleData(roleDetails) {
+      if (roleDetails[1] != 'true') {
+        this.$router.push({ name: 'LViewSkills', params: { roleID: roleDetails[0], rolename: roleDetails[2] } });
+      }
     },
   },
   mounted() {
